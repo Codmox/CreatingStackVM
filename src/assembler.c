1705748bd70a0aed73e50efefed6292be7de4f7a
Ashwin <ashsanthosh8@gmail.com>
2025-11-04 07:41:22 +0530

    added skeleton code

--- src/assembler.c
+++ src/assembler.c
@@ -23,50 +23,14 @@ InstructionMapping register_table[] = {
 };
 
 int find_opcode(const char *name) {
-    for(int i=0; i<NUM_OF_INSTRUCTIONS; i++){
-        if(strcmp(name,instruction_table[i].name) == 0){
-            return instruction_table[i].opcode;
-        }
-    }
-    return -1;
+    
 }
 
 int find_register(const char *name) {
-    for(int i=0; i<NUM_OF_REGISTERS; i++){
-        if(strcmp(name,register_table[i].name) == 0){
-            return register_table[i].opcode;
-        }
-    }
-    return -1;
+    
 }
 
 int assemble_file(const char *filename, int *program) {
-    FILE *fp = fopen(filename, "r");
-    if(!fp){
-        printf("Error opening the file\n");
-    }
-    char line[128];
-    int size = 0;
-    while(fgets(line, 128, fp)){
-        line[strcspn(line, "\n")] = '\0'; 
-        int opcode;
-        char *instr = strtok(line, " ");
-        char *arg = strtok(NULL, " ");
-        if((opcode = find_opcode(instr)) == -1){
-            return -1;
-        }
-        program[size++] = opcode;
-        while(arg) {
-            int r = find_register(arg);
-            if(r == -1){
-                program[size++] = atoi(arg);
-            }else{
-                program[size++] = r;
-            }
-            arg = strtok(NULL, " ");
-        }
-    }
-    fclose(fp);
-    return size;
+    
 }
 
